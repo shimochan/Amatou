87c449acf89ee9e20451dff0c1d8c8c0e5db0236
taka-2120 <yu_tkhs@icloud.com>
2023-02-25 02:05:41 +0900

    update: 抱えてるストレスの数を表示 (#7)

--- screens/home/HomeScreen.tsx
+++ screens/home/HomeScreen.tsx
@@ -1,44 +1,62 @@
 import React from 'react';
-import { Pressable, StyleSheet, Image } from 'react-native';
+import { Pressable, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { Button, View, Text } from 'react-native';
 import { RootStackParamList, RootTabScreenProps } from '../../types';
 import { ZStack, HStack, VStack, Spacer } from 'react-native-stacks';
 import DefaultStyle from '../../constants/DefaultStyles';
 import * as NativeStack from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import stressListController from '../../hooks/stressList';
 
 
 const HomeScreen = ({ navigation }: NativeStack.NativeStackScreenProps<RootStackParamList, 'Home'>) => {
+  const { stressList, setStressList } = stressListController();
+
+  const getStoneImagePath = (count: number) => {
+    const basePath = "../../assets/images/home_stones/";
+    switch (count) {
+      case 0: return require(basePath + "00.jpeg");
+      case 1: return require(basePath + "01.jpg");
+      case 2: return require(basePath + "02.jpg");
+      case 3: return require(basePath + "03.jpg");
+      case 4: return require(basePath + "04.jpg");
+      case 5: return require(basePath + "05.jpg");
+      case 6: return require(basePath + "06.png");
+      case 7: return require(basePath + "07.jpg");
+      default: return require(basePath + "many.png");
+    }
+  }
+
   return (
     <SafeAreaView style={DefaultStyle.safeAreaBackground}>
       <VStack spacing={15} style={DefaultStyle.fullHeight}>
         <Text style={DefaultStyle.title}>抱えているストレス</Text>
-        <Text style={DefaultStyle.title2}>0こ</Text>
+        <Text style={DefaultStyle.title2}>{stressList.length}こ</Text>
 
         <Spacer />
 
-        <Image source={require("../../assets/images/home_stones/00.jpeg")} style={DefaultStyle.homeImage} />
+        <Image source={getStoneImagePath(stressList.length)} style={DefaultStyle.homeImage} />
 
         <Spacer />
 
-        <Pressable onPress={() => navigation.push('StressSelect')} style={DefaultStyle.largeButton}>
+        <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.push('StressSelect')} style={DefaultStyle.largeButton}>
           <ZStack style={DefaultStyle.fill}>
             <Text style={[DefaultStyle.title2, {color: "#fff"}]}>ストレス一覧</Text>
           </ZStack>
-        </Pressable>
+        </TouchableOpacity>
 
         <HStack>
           <Spacer />
 
-          <Pressable onPress={() => navigation.push('History')} style={[DefaultStyle.smallButton, { backgroundColor: '#9BCDA0' }]}>
+          <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.push('History')} style={[DefaultStyle.smallButton, { backgroundColor: '#9BCDA0' }]}>
             <Text>りれき</Text>
-          </Pressable>
+          </TouchableOpacity>
 
           <Spacer />
 
-          <Pressable onPress={() => navigation.push('AddStress')}  style={[DefaultStyle.smallButton, { backgroundColor: '#C09BCD' }]}>
+          <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.push('AddStress')}  style={[DefaultStyle.smallButton, { backgroundColor: '#C09BCD' }]}>
             <Text>ついか</Text>
-          </Pressable>
+          </TouchableOpacity>
 
           <Spacer />
         </HStack>

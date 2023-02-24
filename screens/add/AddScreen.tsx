837567cb0d8a9bd4195a59e5fa84452914eae9bd
shimochan <keita.s0412@icloud.com>
2023-02-24 18:40:59 +0900

    Add textbox

--- screens/add/AddScreen.tsx
+++ screens/add/AddScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { SafeAreaView,StyleSheet,TextInput } from 'react-native';
 import { View } from '../../components/Themed';
 import React from 'react';
 import {  Button } from 'react-native';
@@ -6,8 +6,23 @@ import * as NativeStack from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../types';
 
 export default function AddStress({ navigation }: NativeStack.NativeStackScreenProps<RootStackParamList, 'AddStress'>) {
+  
+    const [text, onChangeText] = React.useState('Useless Text');
+    const [number, onChangeNumber] = React.useState('');
   return (
     <View style={styles.container}>
+       <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
       <Button color = "green" title='ホーム画面へ' onPress={() => navigation.goBack()} />
     </View>
   );
@@ -28,4 +43,14 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
+
+
+
+ 

fb251cf8f856b126cc0d529b8bf6e8a83cf7e04e
taka-2120 <yu_tkhs@icloud.com>
2023-02-25 02:29:59 +0900

    fix: formats

--- screens/solutions/SolutionScreen.tsx
+++ screens/solutions/SolutionScreen.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { View } from '../../components/Themed';
 import React from 'react';
-import {  Button, Image, Text } from 'react-native';
+import { Button, Image, Text } from 'react-native';
 import { HStack, Spacer } from 'react-native-flex-layout';
 import * as NativeStack from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../types';
@@ -11,32 +11,32 @@ export default function SolutionSelect({ navigation }: NativeStack.NativeStackSc
     <View style={styles.container}>
       <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.push("CutAction")} style={styles.imageOuter}>
         <HStack>
-          <Spacer/>
+          <Spacer />
           <Text style={styles.text}>斬る</Text>
-          <Spacer/>
-          <Image source = {require("../../assets/images/kiruyatu.png")} style={styles.imagesize}/>
-          <Spacer/>
+          <Spacer />
+          <Image source={require("../../assets/images/kiruyatu.png")} style={styles.imagesize} />
+          <Spacer />
         </HStack>
       </TouchableOpacity>
       <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.push("Jogging")} style={styles.imageOuter}>
-      <HStack>
-          <Spacer/>
+        <HStack>
+          <Spacer />
           <Text style={styles.text}>走る</Text>
-          <Spacer/>
-          <Image source = {require("../../assets/images/sport_jogging_woman.png")} style={styles.imagesize}/>
-          <Spacer/>
+          <Spacer />
+          <Image source={require("../../assets/images/sport_jogging_woman.png")} style={styles.imagesize} />
+          <Spacer />
         </HStack>
       </TouchableOpacity>
       <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.push("PuchiPuchi")} style={styles.imageOuter}>
         <HStack>
-          <Spacer/>
+          <Spacer />
           <Text style={styles.text}>潰す</Text>
-          <Spacer/>
-          <Image source = {require("../../assets/images/bakuhatsu.png")} style={styles.imagesize}/>
-          <Spacer/>
+          <Spacer />
+          <Image source={require("../../assets/images/bakuhatsu.png")} style={styles.imagesize} />
+          <Spacer />
         </HStack>
       </TouchableOpacity>
-      <Button color = "green" title='ホーム画面へ' onPress={() => navigation.goBack()}/>
+      <Button color="green" title='ホーム画面へ' onPress={() => navigation.goBack()} />
     </View>
   );
 }

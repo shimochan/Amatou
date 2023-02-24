fb68dd3519bc4af9a8e5a878f1589f9d94f3e223
K-Ryo-ta <studyprograming51@gmail.com>
2023-02-25 00:29:55 +0900

    fix:solutionscreen

--- screens/solutions/SolutionScreen.tsx
+++ screens/solutions/SolutionScreen.tsx
@@ -1,25 +1,42 @@
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { View } from '../../components/Themed';
 import React from 'react';
-import {  Button } from 'react-native';
+import {  Button, Image, Text } from 'react-native';
+import { HStack, Spacer } from 'react-native-flex-layout';
 import * as NativeStack from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../types';
-import { Image} from 'react-native';
 
 export default function SolutionSelect({ navigation }: NativeStack.NativeStackScreenProps<RootStackParamList, 'SolutionSelect'>) {
   return (
     <View style={styles.container}>
-      <TouchableOpacity   activeOpacity={0.5} onPress={() => navigation.push("CutAction")}>
-        <Image source = {require("../../assets/images/kiruyatu.png")} style={styles.imagesize}/>
+      <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.push("CutAction")} style={styles.imageOuter}>
+        <HStack>
+          <Spacer/>
+          <Text style={styles.text}>斬る</Text>
+          <Spacer/>
+          <Image source = {require("../../assets/images/kiruyatu.png")} style={styles.imagesize}/>
+          <Spacer/>
+        </HStack>
       </TouchableOpacity>
-      <TouchableOpacity   activeOpacity={0.5} onPress={() => navigation.push("Jogging")}>
-        <Image source = {require("../../assets/images/sport_jogging_man_cap.png")} style={styles.imagesize}/>
+      <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.push("Jogging")} style={styles.imageOuter}>
+      <HStack>
+          <Spacer/>
+          <Text style={styles.text}>走る</Text>
+          <Spacer/>
+          <Image source = {require("../../assets/images/sport_jogging_woman.png")} style={styles.imagesize}/>
+          <Spacer/>
+        </HStack>
       </TouchableOpacity>
-      <TouchableOpacity   activeOpacity={0.5} onPress={() => navigation.push("PuchiPuchi")}>
-        <Image source = {require("../../assets/images/bakuhatsu.png")} style={styles.imagesize}/>
+      <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.push("PuchiPuchi")} style={styles.imageOuter}>
+        <HStack>
+          <Spacer/>
+          <Text style={styles.text}>潰す</Text>
+          <Spacer/>
+          <Image source = {require("../../assets/images/bakuhatsu.png")} style={styles.imagesize}/>
+          <Spacer/>
+        </HStack>
       </TouchableOpacity>
-      <Button color = "green" title='ホーム画面へ' onPress={() => navigation.goBack()} />
-
+      <Button color = "green" title='ホーム画面へ' onPress={() => navigation.goBack()}/>
     </View>
   );
 }
@@ -39,10 +56,22 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  imageOuter: {
+    width: '90%',
+    height: 100,
+    borderRadius: 20,
+    backgroundColor: "lightblue",
+    margin: 10,
+  },
   imagesize: {
-    alignItems: 'center',
+    height: 100,
     width: 100,
-    height: 100
-
+    resizeMode: 'contain',
+    // overflow: "hidden",
+  },
+  text: {
+    fontSize: 40,
+    alignItems: "center",
+    padding: 30
   }
 });

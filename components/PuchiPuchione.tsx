0aeebaf044390d8cac11adbca588ab5cc7108635
K-Ryo-ta <studyprograming51@gmail.com>
2023-02-25 14:41:27 +0900

    complete:puchipuchi

---
+++ components/PuchiPuchione.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { View } from '../components/Themed';
+import React from 'react';
+import {  Button } from 'react-native';
+import * as Haptics from 'expo-haptics';
+import * as NativeStack from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types';
+import { useState } from 'react';
+import { Image} from 'react-native';
+
+export default function PuchiPuchione(key: number) {
+  const imagepuchipuchi2 = require("../assets/images/puchipuchitubushitaato.png")
+  const [imagepuchipuchi, setimagepuchipuchi] = useState(require("../assets/images/puchipuchitubusumae.png"))
+  let puchipuchicount = 0
+  const puchipuchiimage = () => {
+    if(puchipuchicount === 0){
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
+      setimagepuchipuchi(imagepuchipuchi2)
+      puchipuchicount++;
+    }
+  }
+      
+  return (
+      <TouchableOpacity  activeOpacity={0.5} key={key} onPress={puchipuchiimage} style={{width: '20%'}}>
+        <Image source={imagepuchipuchi} style = {styles.imagestyle} />
+      </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 14,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  imagestyle:{
+    width: '100%',
+    resizeMode: 'contain',
+  },
+  touchableOpacitystyle:{
+    display:'flex'
+  }
+});
--- screens/solutions/puchi_puchi/PuchipuchiScreen.tsx
+++ screens/solutions/puchi_puchi/PuchipuchiScreen.tsx
@@ -1,30 +1,41 @@
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { View } from '../../../components/Themed';
 import React from 'react';
-import {  Button } from 'react-native';
+import { Button } from 'react-native';
 import * as Haptics from 'expo-haptics';
 import * as NativeStack from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../../types';
 import { useState } from 'react';
-import { Image} from 'react-native';
-
+import PuchiPuchione from '../../../components/PuchiPuchione'
+import { HStack } from 'react-native-stacks';
 
 export default function PuchiPuchi({ navigation }: NativeStack.NativeStackScreenProps<RootStackParamList, 'PuchiPuchi'>) {
-  const initialpuchipuchi = require("../../../assets/images/puchipuchitubusumae.png")
-  const imagepuchipuchi2 = require("../../../assets/images/puchipuchitubushitaato.png")
   
-  const [imagepuchipuchi, setimagepuchipuchi] = useState(initialpuchipuchi)
-  const puchipuchiimage = () => {
-    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
-    setimagepuchipuchi(imagepuchipuchi2)
+  const items = [];
+
+  for(let v = 0; v < 6; v++) {
+    items.push(hPuchiPuchi(v));
   }
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity  activeOpacity={0.5} onPress={puchipuchiimage}>
-        <Image source={imagepuchipuchi}/>
-      </TouchableOpacity>
-        <Button color = "green" title='ホーム画面へ' onPress={() => navigation.goBack()} />
+      {items}
+      <Button color = "green" title='ホーム画面へ' onPress={() => navigation.goBack()} />
     </View>
+  );
+}
+
+function hPuchiPuchi(vKey: number) {
+  const hItems = [];
+
+  for (let h = 0; h < 5; h++) {
+    hItems.push(PuchiPuchione(vKey + h))
+  }
+
+  return (
+    <HStack>
+      {hItems}
+    </HStack>
   );
 }
 
